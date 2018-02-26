f3af76265edb959ca86ff61c7921693a5d27ce54
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2018-02-26 15:33:38 +0400

    msdb: check db entries on creation

--- test/db-test.js
+++ test/db-test.js
@@ -3,13 +3,34 @@
 
 'use strict';
 
+const assert = require('./util/assert');
 const MultisigDB = require('../lib/multisigdb');
+const layout = require('../lib/layout');
 
 describe('Multisig Database', function () {
   it('should open database', async () => {
-    const db = new MultisigDB();
+    const msdb = new MultisigDB({
+      client: {}
+    });
 
-    await db.open();
-    await db.close();
+    await msdb.open();
+    await msdb.close();
+  });
+
+  it('should create version entry', async () => {
+    const msdb = new MultisigDB({
+      client: {}
+    });
+    const db = msdb.db;
+
+    await msdb.open();
+
+    const version = await db.get(layout.V.build());
+    const flags = await db.get(layout.O.build());
+
+    assert(Buffer.isBuffer(version), 'DB must write version.');
+    assert(Buffer.isBuffer(flags), 'DB must write flags.');
+
+    await msdb.close();
   });
 });
