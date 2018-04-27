ed4b19d1fa042a0be4ab54015ba446ca41e55825
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2018-04-27 18:20:16 +0400

    proposal-test: add more proposal tests * Test approval * Test rejection * Test forceRejection

--- test/proposal-test.js
+++ test/proposal-test.js
@@ -5,6 +5,7 @@
 
 const assert = require('./util/assert');
 const Proposal = require('../lib/proposal');
+const Cosigner = require('../lib/cosigner');
 
 const TEST_OPTIONS = {
   id: 1,
@@ -44,5 +45,104 @@ describe('Proposal', function () {
     proposal1.n = proposal.n;
 
     assert.strictEqual(proposal.equals(proposal1), true);
+  });
+
+  it('should reject proposal', () => {
+    const proposal = Proposal.fromOptions(TEST_OPTIONS);
+    const cosigner1 = Cosigner.fromOptions({
+      id: 0,
+      name: 'cosigner1'
+    });
+
+    const cosigner2 = Cosigner.fromOptions({
+      id: 1,
+      name: 'cosigner2'
+    });
+
+    proposal.reject(cosigner1);
+
+    assert.strictEqual(proposal.rejections.length, 1);
+    assert.strictEqual(proposal.status, Proposal.status.PROGRESS);
+    assert.strictEqual(proposal.rejections[0], cosigner1.id);
+
+    let err;
+    try {
+      proposal.reject(cosigner1);
+    } catch (e) {
+      err = e;
+    }
+
+    assert(err);
+    assert.strictEqual(err.message, 'Cosigner already rejected.');
+
+    proposal.reject(cosigner2);
+
+    assert.strictEqual(proposal.rejections.length, 2);
+    assert.strictEqual(proposal.status, Proposal.status.REJECTED);
+    assert.deepStrictEqual(proposal.rejections, [0, 1]);
+  });
+
+  it('should approve proposal', () => {
+    const proposal = Proposal.fromOptions(TEST_OPTIONS);
+    const cosigner1 = Cosigner.fromOptions({
+      id: 0,
+      name: 'cosigner1'
+    });
+
+    const cosigner2 = Cosigner.fromOptions({
+      id: 1,
+      name: 'cosigner2'
+    });
+
+    proposal.approve(cosigner1);
+
+    assert.strictEqual(proposal.approvals.length, 1);
+    assert.strictEqual(proposal.status, Proposal.status.PROGRESS);
+    assert.strictEqual(proposal.approvals[0], cosigner1.id);
+
+    let err;
+    try {
+      proposal.approve(cosigner1);
+    } catch (e) {
+      err = e;
+    }
+
+    assert(err);
+    assert.strictEqual(err.message, 'Cosigner already approved.');
+
+    proposal.approve(cosigner2);
+
+    assert.strictEqual(proposal.approvals.length, 2);
+    assert.strictEqual(proposal.status, Proposal.status.APPROVED);
+    assert.deepStrictEqual(proposal.approvals, [0, 1]);
+  });
+
+  it('should force reject proposal', () => {
+    const proposal = Proposal.fromOptions(TEST_OPTIONS);
+
+    let err;
+
+    try {
+      proposal.forceReject(Proposal.status.APPROVED);
+    } catch (e) {
+      err = e;
+    }
+
+    assert(err);
+    assert(err.message, 'status needs to be a rejection.');
+
+    proposal.forceReject(Proposal.status.DBLSPEND);
+
+    assert.strictEqual(proposal.status, Proposal.status.DBLSPEND);
+
+    err = null;
+    try {
+      proposal.forceReject(Proposal.status.REJECTED);
+    } catch (e) {
+      err = e;
+    }
+
+    assert(err);
+    assert.strictEqual(err.message, 'Can not reject non pending proposal.');
   });
 });
